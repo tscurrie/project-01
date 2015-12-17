c4aff7e46d3e77126d47770b596cbf34fc205141
tscurrie <tscurrie85@gmail.com>
2015-12-17 11:53:41 -0800

    added heroku

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
  var mongoose = require('mongoose'),
  	Workout = require('./workout.js'),
  	Exercise = require('./exercise.js');
-  mongoose.connect('mongodb://localhost/project-01');
+  mongoose.connect( process.env.MONGOLAB_URI ||
+                    process.env.MONGOHQ_URL ||
+                    'mongodb://localhost/project-01'); 
 
 module.exports.Workout = Workout;
 module.exports.Exercise = Exercise;
